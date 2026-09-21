82a7836b7b71ca1881523dc3874094378ca66cc7
AD?M <ajerouassiadam@gmail.com>
2026-09-21 17:56:04 +0000

    finishing bankAccount class

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,26 +4,47 @@ public class BankAccount {
 
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
+    int idx = 0;
 
     public BankAccount(String name, int startingBalance){
-
+            this.name = name;
+            this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
+        if (amount <= 0) {
+            System.out.println("You cannot diposit a negative ammount.");
+            return;
+        }
 
+        currentBalance += amount;
+        transactions[idx] = (+amount);
+        idx++;
+
+        System.out.println(name + " diposit : " + amount + ". New balance : " + currentBalance);
     }
 
     public void withdraw(double amount){
+        if (amount > currentBalance) {
+            System.out.println("This passes your balance. Unseccessful withdraw.");
+            return;
+        }
 
+        currentBalance -= amount;
+        transactions[idx] = (-amount);
     }
 
     public void displayTransactions(){
+        System.out.println("Your list of transaction is the following: ");
 
+        for(double t : transactions) {
+            System.out.println(t);
+        }
     }
 
     public void displayBalance(){
-
+        System.out.println("The current balance of the account is : " + currentBalance);
     }
 
     public static void main(String[] args) {
